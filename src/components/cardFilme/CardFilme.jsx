24dbdfcc823e7e2ae159a101e181aaeed7c6f3a6
gustta11 <gustavokeven50@gmail.com>
2025-12-01 14:10:31 -0300

    Adicionando class no Card

--- src/components/cardFilme/CardFilme.jsx
+++ src/components/cardFilme/CardFilme.jsx
@@ -2,7 +2,7 @@ import Style from './cardFilme.module.scss'
 
 const CardFilme = (props) => {
   return (
-    <article>
+    <article className={Style}>
         <img src={props.src} alt={props.alt} />
         <h2 className="h2_card">{props.titulo}</h2>
         <p>{props.descricao}</p>
